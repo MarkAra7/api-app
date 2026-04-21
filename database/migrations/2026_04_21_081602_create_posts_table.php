319784e3712867c0f2b56b548611d6ee6c813eda
marara <arajums4@gmail.com>
2026-04-21 11:23:20 +0300

    fixed foreign key in post table

--- database/migrations/2026_04_21_081602_create_posts_table.php
+++ database/migrations/2026_04_21_081602_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('body');
-            $table->foreignIdFor(User::class);
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
